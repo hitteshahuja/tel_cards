d27d79ac5b914f35a3deace4c424afcdbe34385e
Hittesh Ahuja <h.ahuja@bath.ac.uk>
2019-02-19 10:36:06 +0000

    - Added new pages - Added images

---
+++ src/templates/Default.svelte
@@ -0,0 +1,44 @@
+<div class="card-container manual-flip">
+<div class="col-md-12">
+        <div class="bs-callout bs-callout-catalogue clearfix">
+            <div class="icon">
+                <i class="far fa-bookmark" aria-hidden="true"></i>
+            </div>
+            <h4>Case Studies</h4>
+            <p>Case studies go here...</p>
+        </div>
+        <div class="card cataloguepane" data-spy="scroll">
+            <div class="front">
+                <ul class="list-group list-group-flush">
+                    {{#each defaultCards as card}}
+                    <a href="#" class="list-group-item list-group-item-action" on:click="store.set({ currentPage: 'card', currentCard: card.id })">
+                        <div class="applist">
+                            <h5><img style="display: inline; height: 1em; vertical-align: top;" src="{{ card.assets.logo.image }}"> {{ card.name }}</h5>
+                            <p class="categories">
+                                {{#each card.activities as activity}}
+                                <span class="badge activity {{ activity.name }}">{{ activity.name }}</span>&nbsp;
+                                {{/each}}
+                            </p>
+                            <p class="submittercatalogue"><small>Submitted by {{ card.submitter.name }} | {{ card.submitter.title }}</small></p>
+                            <p class="appinfo">{{ card.tagline }}</p>
+                        </div>
+                    </a>
+                    {{/each}}
+                </ul>
+            </div>
+        </div>
+
+</div>
+</div>
+<script>
+    export default {
+        // Show only cards that have default = yes
+computed: {
+    defaultCards:($cards) => {
+        return $cards.filter(card => {
+            return card.default === true
+        })
+    }
+}
+    }
+</script>
---
+++ src/templates/Embed.svelte
@@ -0,0 +1,25 @@
+<div class="card mainCard">
+<div class="container card-holder">
+				<Card :card />
+			</div>
+</div>
+<script>
+import Card from './Card.svelte'
+export default {
+    oncreate() {
+        // Child is positioned absolutely, so this forces container to fill height of child
+        const childHeight = document.querySelector('.card').scrollHeight
+        //document.querySelector('#appgenerator').style.height = `${childHeight-150}px`
+    },
+
+    // Expose the templates as components that can be referened in this template
+    components: {
+        Card
+    },
+
+    // Compute the card object by indexing the currentCard in the cards array
+    computed: {
+        card: ($cards, $currentCard) => $cards.find(card => card.id === $currentCard)
+    }
+}
+</script>
